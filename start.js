87b6abd238cb49a20d37bbf3a7be820315c28be5
tinchogob <martin.gonzalez@mercadolibre.com>
2015-01-07 17:35:50 -0300

    Change binding port

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
 var app = require('./app.js');
 
-app.listen(3000, function() {
-	console.log('running on port 3000');
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+	console.log('running on port %s', port);
 });
